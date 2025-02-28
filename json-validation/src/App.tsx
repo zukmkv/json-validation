23641c716bdc8eaebf94da02e1d8f9963bcfe6fe
Alexander MKV <zuk-m@ya.ru>
2025-02-28 21:54:55 +0400

    Visualize JSON Schemas

--- json-validation/src/App.tsx
+++ json-validation/src/App.tsx
@@ -1,31 +1,69 @@
-import { useState } from 'react'
-import './App.css'
-import { fetchMockData, RANDOM_USER_URL } from './api'
-import { Loader } from './components/loader';
+import { useState } from "react";
+import "./App.css";
+import {
+  fetchMockData,
+  JSON_PLACEHOLDER_SCHEMA_URL,
+  JSON_PLACEHOLDER_URL,
+  RANDOM_USER_SCHEMA_URL,
+  RANDOM_USER_URL,
+} from "./api";
+import { Loader } from "./components/Loader";
 
 function App() {
+  const [state, setState] = useState({
+    json: "",
+    schema: "",
+    isLoading: false,
+  });
 
-  const [json, setJson] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
+  async function handleFetch(jsonUrl: string, schemaUrl: string) {
+    setState({ json: "", schema: "", isLoading: true });
 
-  async function handleClick(url: string) {
-    setIsLoading(true);
-    setJson("");
-    const result = await fetchMockData(url);
-    setIsLoading(false);
-    setJson(JSON.stringify(result, null, 2));
+    const [jsonResult, schemaResult] = await Promise.allSettled([
+      fetchMockData(jsonUrl),
+      fetchMockData(schemaUrl),
+    ]);
+
+    setState({
+      isLoading: false,
+      json: setJsonState(jsonResult, 'Ошибка загрузки JSON'),
+      schema: setJsonState(schemaResult, 'Ошибка загрузки схемы'),
+    });
   }
 
   return (
     <>
-      <button onClick={() => handleClick(RANDOM_USER_URL)}>
-        Fetch random JSON
-      </button>
-      {isLoading ? <Loader /> : <pre>
-        {json}
-      </pre>}
+      <div className="fetch-button-wrapper">
+        <button onClick={() => handleFetch(RANDOM_USER_URL, RANDOM_USER_SCHEMA_URL)} disabled={state.isLoading}>
+          Загрузить данные
+        </button>
+        {state.isLoading && <Loader />}
+      </div>
+
+      <button onClick={() => { }}>Проверить</button>
+
+      <div className="flex-wrapper">
+        {state.json && (
+          <div>
+            <h2>JSON</h2>
+            <pre>{state.json}</pre>
+          </div>
+        )}
+        {state.schema && (
+          <div>
+            <h2>JSON Schema</h2>
+            <pre>{state.schema}</pre>
+          </div>
+        )}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+const setJsonState = (json: PromiseSettledResult<string>, error: string) => {
+  return json.status === "fulfilled"
+    ? JSON.stringify(json.value, null, 2)
+    : `${error}: ${json.reason}`;
+}
+
+export default App;
--- json-validation/src/api/index.ts
+++ json-validation/src/api/index.ts
@@ -1,9 +1,16 @@
-export const MOCKAPI_URL = "https://67c1b98861d8935867e40be7.mockapi.io/api/users";
 export const RANDOM_USER_URL = "https://randomuser.me/api";
 export const JSON_PLACEHOLDER_URL = "https://jsonplaceholder.typicode.com/todos/1";
 
+export const RANDOM_USER_SCHEMA_URL = "/jsonSchemas/randomUser.json"
+export const JSON_PLACEHOLDER_SCHEMA_URL = "/jsonSchemas/jsonPlaceholder.json";
+
 export const fetchMockData = async (url: string): Promise<string> => {
-  const response = await fetch(url);
-  const json = await response.json()
-  return json;
-}
+  try {
+    const response = await fetch(url);
+    const json = await response.json();
+    return json;
+  } catch (err) {
+    console.error(err);
+    return '';
+  }
+};
